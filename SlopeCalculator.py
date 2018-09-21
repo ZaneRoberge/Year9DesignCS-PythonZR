be1f4d522c04ec00161ce527deafcf0af324d213
Zane Roberge <zane.roberge@y22-Design-ZaneRoberge.local>
2018-09-21 13:47:03 -0400

    Slope Calculator update

--- SlopeCalculator.py
+++ SlopeCalculator.py
@@ -1,1 +1,27 @@
+import os
+
+
 print("Slope Calculator:")
+os.system("say Hello")
+#Input
+x1 = input("Input x1: ")
+x1 = int(x1)
+
+y1 = input("Input y1: ")
+y1 = int(y1)
+	
+x2 = input("Input x2: ")
+x2 = int(x2)
+
+y2 = input("Input y2: ")
+y2 = int(y2)
+#Process
+rise = x2 - x1
+run = y2 - y1
+
+
+fSlope = rise/run
+#Output
+print("Your slope is m="+str(rise)+"/"+str(run))
+print("Yours slope as a decimal is " +str(fSlope))
+
